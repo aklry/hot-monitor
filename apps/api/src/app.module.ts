7f9613e146a1c8ba2613fc2d559d0bc5c2694710
aklry <3265242862@qq.com>
2026-06-18 22:31:11 +0800

    feat: add prisma data layer

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 import { ScheduleModule } from "@nestjs/schedule"
 import { configuration } from "./config/configuration"
+import { DatabaseModule } from "./database/database.module"
 import { HealthController } from "./health.controller"
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    DatabaseModule,
     ScheduleModule.forRoot()
   ],
   controllers: [HealthController]
